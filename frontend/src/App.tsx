8ad2a7a0c1d5ed9e46a21acd3ad311066a034299
suryadwd <surajdwivedi644@gmail.com>
2025-05-27 00:27:17 +0530

    adding the items ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css"
 import Login from "./components/Login"
 import { Routes, Route } from 'react-router-dom';
 import Signup from "./components/Signup";
-
+import Display from "./components/Display";
 const App = () => {
   return (
     <div className="bg-gradient-to-r from-black to-blue-600 text-white min-h-screen flex items-center justify-center">
      <Routes>
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/display" element={<Display />} />
      </Routes>
     </div>
   )
---
+++ frontend/src/components/Display.tsx
@@ -0,0 +1,32 @@
+const Display = () => {
+  return (
+    <div className="flex flex-wrap gap-3">
+    <div className=" flex flex-col items-center justify-center border-4 border-blue-500 rounded-lg p-1 bg-white text-black">
+      <div className="">
+        <img
+          className=" "
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH6JXA-II3rXMJZxRq89DLqxBRpTWxdP1YZQ&s"
+          alt="Display"
+        />
+      </div>
+
+      <div className="flex flex-col items-center justify-center relative p-2">
+        <div className="mb-[-1] flex items-center justify-between w-full">
+          <div className="text-xl font-extrabold  ">T-shirt</div>
+          <div className="text-xl font-extrabold">₹ 499</div>
+        </div>
+        <div className="font-light text-gray-400 mb-1">
+          T shirt blue in color hot fabric strechable
+        </div>
+        <div className="flex items-center justify-evenly w-full gap-2">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy Now </button>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Update </button>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete </button>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Display;
