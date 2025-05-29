cd22b1964f9e839b2a19dce29f8a63bed85b3b28
suryadwd <surajdwivedi644@gmail.com>
2025-05-29 09:23:04 +0530

    protection routes

--- backend/routes/product.routes.ts
+++ backend/routes/product.routes.ts
@@ -5,12 +5,13 @@ import {
   getProducts,
   updateProduct,
 } from "../controller/product.controller.js";
+import { protectRoute } from "../utils/middleware.js";
 
 const router = Router();
 
-router.post("/create", createProduct);
-router.get("/", getProducts);
-router.post("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/protectRoute/create", createProduct);
+router.get("/protectRoute/", getProducts);
+router.post("/protectRoute/:id", updateProduct);
+router.delete("/protectRoute/:id", deleteProduct);
 
 export default router;
