3c2636ae898fa07b58553b55efcf2897fd3a552a
suryadwd <surajdwivedi644@gmail.com>
2025-06-05 18:50:13 +0530

    nothing

--- backend/routes/user.routes.ts
+++ backend/routes/user.routes.ts
@@ -1,11 +1,18 @@
-import express from 'express';
-import { getUserInfo, login, logout, register } from '../controller/user.controller.js';
+import express from "express";
+import {
+  getUserInfo,
+  login,
+  logout,
+  register,
+} from "../controller/user.controller.js";
 import { protectRoute } from "../utils/middleware.js";
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/user', protectRoute, getUserInfo);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/user", protectRoute, getUserInfo);
+
+// getting more now
 
 export default router;
